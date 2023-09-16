f707e16a0b95bc52de8ac41cdb398010e8e76fa6
mathiaswittig01 <mwittig98@gmail.com>
2023-09-16 16:58:52 +0200

    Can open a predifined website when clicking a .link on win

--- Flutter/LinkOpener002/LinkOpener002/Program.cs
+++ Flutter/LinkOpener002/LinkOpener002/Program.cs
@@ -14,39 +14,17 @@ namespace LinkOpener
 
             Console.WriteLine("Hello");
 
-            // Keep the console window open until a key is pressed.
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-        }
-
-        private static void RegisterFileExtension()
-        {
-            string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            string extension = ".link";
-            string fileType = "LinkOpener";
-            string description = "Link Opener";
-
-            // Check if the file extension is already registered.
-            RegistryKey key = Registry.ClassesRoot.OpenSubKey(extension);
-            if (key != null && key.GetValue(null) as string == fileType)
+            // Open the URL in the default web browser
+            Process.Start(new ProcessStartInfo
             {
-                // File extension is already registered.
-                return;
-            }
-            key?.Close();
+                FileName = "https://mathias-wittig.com/",
+                UseShellExecute = true
+            });
 
-            // Register the file extension.
-            key = Registry.ClassesRoot.CreateSubKey(extension);
-            key.SetValue("", fileType);
-            key.Close();
 
-            key = Registry.ClassesRoot.CreateSubKey(fileType);
-            key.SetValue("", description);
-            key.Close();
-
-            key = Registry.ClassesRoot.CreateSubKey($@"{fileType}\shell\open\command");
-            key.SetValue("", $@"""{exePath}"" ""%1""");
-            key.Close();
+            // Keep the console window open until a key is pressed.
+            //Console.WriteLine("Press any key to exit...");
+            //Console.ReadKey();
         }
 
 
