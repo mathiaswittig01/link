7903e1b80576f4a3f9d44f6b01145cdd4ea106e4
mathiaswittig01 <mwittig98@gmail.com>
2023-09-16 21:08:36 +0200

    created a firste release msi

--- Flutter/LinkOpener002/LinkOpener002/Program.cs
+++ Flutter/LinkOpener002/LinkOpener002/Program.cs
@@ -11,12 +11,6 @@ namespace LinkOpener
     {
         static void Main(string[] args)
         {
-            // Register the file extension if not already registered.
-            //RegisterFileExtension();
-
-            Console.WriteLine("Hello");
-
-
 
             if (args.Length > 0)
             {
@@ -59,15 +53,6 @@ namespace LinkOpener
             }
 
 
-            // Open the URL in the default web browser
-            //Process.Start(new ProcessStartInfo
-            //{
-            //    FileName = "https://mathias-wittig.com/",
-            //    UseShellExecute = true
-            //});
-
-
-            // Keep the console window open until a key is pressed.
             //Console.WriteLine("Press any key to exit...");
             //Console.ReadKey();
         }
